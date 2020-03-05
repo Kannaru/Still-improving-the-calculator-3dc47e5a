ce27a64c50df1b375b9f2b5e4f638897446007d2
Mark Verrips <2083591@talnet.nl>
2020-03-05 14:00:07 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <h1><strong>Calculator</strong></h1>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="number" name="eerstenummer" required>
+        <label><strong>First Number</strong></label><br><br>
+        <input type="number" name="tweedenummer" required>
+        <label><strong>Second Number</strong></label><br><br>
+
+        <input name="berekening" type="submit" value="Add">
+        <input name="berekening" type="submit" value="Subtract">
+        <input name="berekening" type="submit" value="Multiply">
+        <input name="berekening" type="submit" value="Divide">
+        <input name="berekening" type="submit" value="Modulo">
+    </form>
+
+    <?php
+
+    $eerstenummer = $_POST['eerstenummer'];
+    $tweedenummer = $_POST['tweedenummer'];
+    $berekening = $_POST['berekening'];
+    $eindnummer = '';
+
+    switch ($berekening) {
+        case "Add":
+            $eindnummer = $eerstenummer + $tweedenummer;
+            break;
+        case "Subtract":
+            $eindnummer = $eerstenummer - $tweedenummer;
+            break;
+        case "Multiply":
+            $eindnummer = $eerstenummer * $tweedenummer;
+            break;
+        case "Divide":
+            $eindnummer = $eerstenummer / $tweedenummer;
+        case "Modulo":
+            $eindnummer = $eerstenummer % $tweedenummer;
+    }
+    echo $eindnummer;
+    ?>
+</body>
+
+</html>
